7a5951c17518461ef5e72df0c92c809e2284f9cb
Mohamed-7-Sayed <m.sayed21010@gmail.com>
2023-12-25 17:56:11 +0200

    testt

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,7 +72,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'country' => $data['country'],
-            'job' => $data['job'],
+            'jobs' => $data['job'],
             'image' => $this->uploadFile($data['Image'], 'imgs'),
             'password' => Hash::make($data['password']),
         ]);
